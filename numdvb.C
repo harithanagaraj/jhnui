76933a05b9fd99e57a26ec3de0de0a1c027139df
harithanagaraj <36696201+harithanagaraj@users.noreply.github.com>
2018-03-28 15:50:10 +0530

    prgm

---
+++ numdvb.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+ 
+int main()
+{
+   int a, b, c;
+ 
+   printf("Enter two numbers to add\n");
+   scanf("%d%d", &a, &b);
+   {
+   for(i=0;i++;i<7);
+ 
+   c = a + b;
+ 
+   printf("Sum of the numbers = %d\n", c);
+ 
+   return 0;
+}
